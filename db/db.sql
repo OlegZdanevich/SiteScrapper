e8943ab7f2b28cc70d7cf1eb18fb314b32032b7c
Oleg Zdanevich <ya.zdanevich@yandex.ru>
2019-11-13 16:30:19 +0300

    Pre-final version
    Wasn't counted on online+afisha data, counted only on afisha data

--- db/db.sql
+++ db/db.sql
@@ -16,10 +16,47 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: actor; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.actor (
+    id bigint NOT NULL,
+    name character varying(256),
+    additional_information character varying(256)
+);
+
+
+ALTER TABLE public.actor OWNER TO postgres;
+
+--
+-- Name: country; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.country (
+    id bigint NOT NULL,
+    country character varying(256)
+);
+
+
+ALTER TABLE public.country OWNER TO postgres;
+
+--
+-- Name: director; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.director (
+    id bigint NOT NULL,
+    name character varying(256),
+    additional_information character varying(256)
+);
+
+
+ALTER TABLE public.director OWNER TO postgres;
 
 --
 -- Name: genre; Type: TABLE; Schema: public; Owner: postgres
@@ -55,6 +92,42 @@ CREATE TABLE public.movie (
 ALTER TABLE public.movie OWNER TO postgres;
 
 --
+-- Name: movie_actor_link; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movie_actor_link (
+    movie_id bigint NOT NULL,
+    actor_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.movie_actor_link OWNER TO postgres;
+
+--
+-- Name: movie_country_link; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movie_country_link (
+    movie_id bigint NOT NULL,
+    country_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.movie_country_link OWNER TO postgres;
+
+--
+-- Name: movie_director_link; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movie_director_link (
+    movie_id bigint NOT NULL,
+    director_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.movie_director_link OWNER TO postgres;
+
+--
 -- Name: movie_genre_link; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -65,6 +138,43 @@ CREATE TABLE public.movie_genre_link (
 
 
 ALTER TABLE public.movie_genre_link OWNER TO postgres;
+
+--
+-- Name: movie_image_link; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movie_image_link (
+    id bigint NOT NULL,
+    url character varying(256) NOT NULL
+);
+
+
+ALTER TABLE public.movie_image_link OWNER TO postgres;
+
+--
+-- Name: movie_rating_link; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movie_rating_link (
+    movie_id bigint NOT NULL,
+    rating_id bigint NOT NULL,
+    value character varying(30)
+);
+
+
+ALTER TABLE public.movie_rating_link OWNER TO postgres;
+
+--
+-- Name: movie_video_link; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movie_video_link (
+    id bigint NOT NULL,
+    url character varying(256) NOT NULL
+);
+
+
+ALTER TABLE public.movie_video_link OWNER TO postgres;
 
 --
 -- Name: movie_year_link; Type: TABLE; Schema: public; Owner: postgres
@@ -79,17 +189,53 @@ CREATE TABLE public.movie_year_link (
 ALTER TABLE public.movie_year_link OWNER TO postgres;
 
 --
+-- Name: rating; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.rating (
+    id bigint NOT NULL,
+    rating character varying(30)
+);
+
+
+ALTER TABLE public.rating OWNER TO postgres;
+
+--
 -- Name: year; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.year (
     id bigint NOT NULL,
-    data integer,
+    year integer,
     url character varying(256)
 );
 
 
 ALTER TABLE public.year OWNER TO postgres;
+
+--
+-- Data for Name: actor; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.actor (id, name, additional_information) FROM stdin;
+\.
+
+
+--
+-- Data for Name: country; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.country (id, country) FROM stdin;
+\.
+
+
+--
+-- Data for Name: director; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.director (id, name, additional_information) FROM stdin;
+\.
+
 
 --
 -- Data for Name: genre; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -108,10 +254,58 @@ COPY public.movie (id, url, name, is_premiere, age_rank, native_film_name, poste
 
 
 --
+-- Data for Name: movie_actor_link; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movie_actor_link (movie_id, actor_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: movie_country_link; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movie_country_link (movie_id, country_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: movie_director_link; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movie_director_link (movie_id, director_id) FROM stdin;
+\.
+
+
+--
 -- Data for Name: movie_genre_link; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.movie_genre_link (movie_id, genre_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: movie_image_link; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movie_image_link (id, url) FROM stdin;
+\.
+
+
+--
+-- Data for Name: movie_rating_link; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movie_rating_link (movie_id, rating_id, value) FROM stdin;
+\.
+
+
+--
+-- Data for Name: movie_video_link; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movie_video_link (id, url) FROM stdin;
 \.
 
 
@@ -124,11 +318,43 @@ COPY public.movie_year_link (movie_id, year_id) FROM stdin;
 
 
 --
+-- Data for Name: rating; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.rating (id, rating) FROM stdin;
+\.
+
+
+--
 -- Data for Name: year; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.year (id, data, url) FROM stdin;
+COPY public.year (id, year, url) FROM stdin;
 \.
+
+
+--
+-- Name: actor actor_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.actor
+    ADD CONSTRAINT actor_pk PRIMARY KEY (id);
+
+
+--
+-- Name: country country_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.country
+    ADD CONSTRAINT country_pk PRIMARY KEY (id);
+
+
+--
+-- Name: director director_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.director
+    ADD CONSTRAINT director_pk PRIMARY KEY (id);
 
 
 --
@@ -140,11 +366,43 @@ ALTER TABLE ONLY public.genre
 
 
 --
+-- Name: movie_actor_link movie_actor_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_actor_link
+    ADD CONSTRAINT movie_actor_link_pk PRIMARY KEY (movie_id, actor_id);
+
+
+--
+-- Name: movie_country_link movie_country_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_country_link
+    ADD CONSTRAINT movie_country_link_pk PRIMARY KEY (movie_id, country_id);
+
+
+--
+-- Name: movie_director_link movie_director_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_director_link
+    ADD CONSTRAINT movie_director_link_pk PRIMARY KEY (movie_id, director_id);
+
+
+--
 -- Name: movie_genre_link movie_genre_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.movie_genre_link
     ADD CONSTRAINT movie_genre_link_pk PRIMARY KEY (movie_id, genre_id);
+
+
+--
+-- Name: movie_image_link movie_picture_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_image_link
+    ADD CONSTRAINT movie_picture_link_pk PRIMARY KEY (id, url);
 
 
 --
@@ -156,6 +414,22 @@ ALTER TABLE ONLY public.movie
 
 
 --
+-- Name: movie_rating_link movie_rating_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_rating_link
+    ADD CONSTRAINT movie_rating_link_pk PRIMARY KEY (movie_id, rating_id);
+
+
+--
+-- Name: movie_video_link movie_video_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_video_link
+    ADD CONSTRAINT movie_video_link_pk PRIMARY KEY (id, url);
+
+
+--
 -- Name: movie_year_link movie_year_link_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -164,11 +438,40 @@ ALTER TABLE ONLY public.movie_year_link
 
 
 --
+-- Name: rating rating_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.rating
+    ADD CONSTRAINT rating_pk PRIMARY KEY (id);
+
+
+--
 -- Name: year year_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.year
     ADD CONSTRAINT year_pk PRIMARY KEY (id);
+
+
+--
+-- Name: actor_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX actor_id_uindex ON public.actor USING btree (id);
+
+
+--
+-- Name: country_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX country_id_uindex ON public.country USING btree (id);
+
+
+--
+-- Name: director_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX director_id_uindex ON public.director USING btree (id);
 
 
 --
@@ -186,10 +489,65 @@ CREATE UNIQUE INDEX movie_id_uindex ON public.movie USING btree (id);
 
 
 --
+-- Name: rating_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX rating_id_uindex ON public.rating USING btree (id);
+
+
+--
 -- Name: year_id_uindex; Type: INDEX; Schema: public; Owner: postgres
 --
 
 CREATE UNIQUE INDEX year_id_uindex ON public.year USING btree (id);
+
+
+--
+-- Name: movie_actor_link movie_actor_link_actor_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_actor_link
+    ADD CONSTRAINT movie_actor_link_actor_id_fk FOREIGN KEY (actor_id) REFERENCES public.actor(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_actor_link movie_actor_link_movie_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_actor_link
+    ADD CONSTRAINT movie_actor_link_movie_id_fk FOREIGN KEY (movie_id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_country_link movie_country_link_country_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_country_link
+    ADD CONSTRAINT movie_country_link_country_id_fk FOREIGN KEY (country_id) REFERENCES public.country(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_country_link movie_country_link_movie_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_country_link
+    ADD CONSTRAINT movie_country_link_movie_id_fk FOREIGN KEY (movie_id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_director_link movie_director_link_director_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_director_link
+    ADD CONSTRAINT movie_director_link_director_id_fk FOREIGN KEY (director_id) REFERENCES public.director(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_director_link movie_director_link_movie_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_director_link
+    ADD CONSTRAINT movie_director_link_movie_id_fk FOREIGN KEY (movie_id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 
 
 --
@@ -206,6 +564,38 @@ ALTER TABLE ONLY public.movie_genre_link
 
 ALTER TABLE ONLY public.movie_genre_link
     ADD CONSTRAINT movie_genre_link_movie_id_fk FOREIGN KEY (movie_id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_image_link movie_picture_link_movie_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_image_link
+    ADD CONSTRAINT movie_picture_link_movie_id_fk FOREIGN KEY (id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_rating_link movie_rating_link_movie_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_rating_link
+    ADD CONSTRAINT movie_rating_link_movie_id_fk FOREIGN KEY (movie_id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_rating_link movie_rating_link_rating_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_rating_link
+    ADD CONSTRAINT movie_rating_link_rating_id_fk FOREIGN KEY (rating_id) REFERENCES public.rating(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+
+--
+-- Name: movie_video_link movie_video_link_movie_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movie_video_link
+    ADD CONSTRAINT movie_video_link_movie_id_fk FOREIGN KEY (id) REFERENCES public.movie(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 
 
 --
